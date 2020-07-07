fe7ff2c32abaf05de52d5193ff861b062dd059b3
Ahmed <ahmednhosna@gmail.com>
2020-07-07 14:26:25 +0200

    Added styles.

--- pages/_error.js
+++ pages/_error.js
@@ -1,15 +1,13 @@
 import Layout from "../components/Layout";
 
-function NotFound() {
+function NotFound({ statusCode }) {
   return (
     <Layout pageTitle="404 Error" docTitle="Page Not Found!">
-      <p>This page could not be found.</p>
-
-      <style jsx global>{`
-        .o-main {
-          text-align: center;
-        }
-      `}</style>
+      {statusCode ? (
+        `Could not load your data: Status Code ${statusCode}`
+      ) : (
+        <p>This page could not be found.</p>
+      )}
     </Layout>
   );
 }
---
+++ pages/blog.js
@@ -0,0 +1,7 @@
+import Layout from "../components/Layout";
+
+function Blog() {
+  return <Layout pageTitle="Latest Articles" docTitle="Blog"></Layout>;
+}
+
+export default Blog;
